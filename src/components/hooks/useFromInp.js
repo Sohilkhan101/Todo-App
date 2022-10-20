6f7c28e272f5e753480faec38613efabbaad8278
sohil khan <sssmewati70@gmail.com>
2022-10-20 11:41:33 +0530

    TodoApp

---
+++ src/components/hooks/useFromInp.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const useFormInp = () => {
+  const [value, setValue] = useState("");
+
+  const setInput = (event) => {
+    setValue(event.target.value);
+  };
+
+  const resetInput = () => {
+    setValue("");
+  };
+
+  return [value, setInput, resetInput];
+};
+
+export default useFormInp;
